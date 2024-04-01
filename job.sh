be003ae4d9368f70aa58f332494575fff6652f2c
pdrebello <pratheekrebello99@gmail.com>
2024-04-01 15:14:41 -0400

    plotting

--- job.sh
+++ job.sh
@@ -1,12 +1,12 @@
 export CUDA_VISIBLE_DEVICES=0,1
-python -m pytest -l -v -k "a4_1_1" &> a4_1_1.txt
-python project/run_data_parallel.py --pytest True --n_epochs 1 &> data_parallel_test.txt
+python -m pytest -l -v -k "a4_1_1" &> outputs/a4_1_1.txt
+python project/run_data_parallel.py --pytest True --n_epochs 1 &> outputs/data_parallel_test.txt
 python -m pytest -l -v -k "a4_1_2" &> a4_1_2.txt
 
-python project/run_data_parallel.py --world_size 1 --batch_size 64 &> data_parallel_1.txt
-python project/run_data_parallel.py --world_size 2 --batch_size 128 &> data_parallel_2.txt
+python project/run_data_parallel.py --world_size 1 --batch_size 64 &> outputs/data_parallel_1.txt
+python project/run_data_parallel.py --world_size 2 --batch_size 128 &> outputs/data_parallel_2.txt
 
 python -m pytest -l -v -k "a4_2_1" &> a4_2_1.txt
 python -m pytest -l -v -k "a4_2_2" &> a4_2_2.txt
-python project/run_pipeline.py --model_parallel_mode='model_parallel' &> model_parallel.txt
-python project/run_pipeline.py --model_parallel_mode='pipeline_parallel' &> pipeline_parallel.txt
+python project/run_pipeline.py --model_parallel_mode='model_parallel' --batch_size=128 --n_epochs=10 &> outputs/model_parallel.txt
+python project/run_pipeline.py --model_parallel_mode='pipeline_parallel' --batch_size=128 --n_epochs=10 &> outputs/pipeline_parallel.txt
